36a9f38f650f46fc6fd8fcddf6c6a149a50644fe
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-05-29 17:12:24 +0530

    methods

---
+++ src/OOPS/methods.java
@@ -0,0 +1,16 @@
+package OOPS;
+import java.util.*;
+public class methods {
+	public int additon(int a,int b) {
+		int c= a+b;
+		return c;
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		methods obj = new methods();
+		int c = obj.additon(a, b);	
+		System.out.println(c);
+	}
+}
