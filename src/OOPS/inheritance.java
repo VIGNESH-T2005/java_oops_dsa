a8273749a7c0edc07cfc2c9af9e1e3c195a0c13f
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-03 22:10:48 +0530

    inheritance

--- src/OOPS/inheritance.java
+++ src/OOPS/inheritance.java
@@ -1,9 +1,31 @@
 package OOPS;
 
+class book{
+	String name;
+	book(String name) {
+		this.name=name;
+	}
+	void bookname() {
+		System.out.println("the name of book is"+ name);
+	}
+}
+class child extends book {
+	child(String name){
+		super(name);
+	}
+	@Override
+	void bookname(){
+		System.out.println("the book is "+ name);
+		}
+}
+
 public class inheritance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		book obj = new book(" focus on what matters");
+		obj.bookname();
+		book obj2= new child("ppa");
+		obj2.bookname();
 
 	}
 
