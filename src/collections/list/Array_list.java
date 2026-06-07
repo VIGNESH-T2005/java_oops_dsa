eb9f13d4bd64411908057aee9451d5acd82f6d14
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-07 23:23:22 +0530

    arraylist

--- src/collections/list/Array_list.java
+++ src/collections/list/Array_list.java
@@ -1,9 +1,13 @@
 package collections.list;
-
+import java.util.*;
 public class Array_list {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  ArrayList<Integer> list = new ArrayList<>();
+	  list.add(3);
+	  list.add(4);
+	  list.add(3);
+	  System.out.println(list);
 
 	}
 
